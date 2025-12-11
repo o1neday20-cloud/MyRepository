76798ae6225298402dbf9b3909fa43f5dc1c63e5
혜원 <@hyewon>
2025-12-11 13:21:04 +0900

    First commit in dev branch

--- GitHW/src/mygit/MyClass.java
+++ GitHW/src/mygit/MyClass.java
@@ -1,7 +1,11 @@
 package mygit;
+
 public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("This is the 2nd version.");
-		}
+		YourClass your = new YourClass();
+		your.sayHello("Everyone");
+	}
+	
 }
